e2f68424a6efc55475aac97c6afcedf88c70162f
Nayaata <n.m.keranova@gmail.com>
2017-03-18 14:27:37 +0200

    Create OpenWeatherMap app project skeleton and edit .gitignore file

---
+++ openweathermap/conf/appc.arrowdb.development.js
@@ -0,0 +1,16 @@
+/**
+ * Generated Arrow DB configuration for development
+ */
+module.exports = {
+  "connectors": {
+    "appc.arrowdb": {
+      "enabled": true,
+      "connector": "appc.arrowdb",
+      "environment": "development",
+      "key": "5YpgzmbBH97geIl6T8hrEaYXHJliQGgT",
+      "baseurl": "https://api.cloud.appcelerator.com",
+      "username": "appc_app_user_dev",
+      "password": "uilP2CQCsRatyYDJsA"
+    }
+  }
+};
---
+++ openweathermap/conf/appc.arrowdb.production.js
@@ -0,0 +1,16 @@
+/**
+ * Generated Arrow DB configuration for production
+ */
+module.exports = {
+  "connectors": {
+    "appc.arrowdb": {
+      "enabled": true,
+      "connector": "appc.arrowdb",
+      "environment": "production",
+      "key": "WHx8Njg44q5pok0z1JlLRAVW3ukfu5TO",
+      "baseurl": "https://api.cloud.appcelerator.com",
+      "username": "appc_app_user",
+      "password": "buBbvP7m2qbvVTdiET"
+    }
+  }
+};
