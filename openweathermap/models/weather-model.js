030744804c7cda2eb47d7f7fef7cdca29549c96c
Nayaata <n.m.keranova@gmail.com>
2017-03-23 23:55:37 +0200

    Updated the model

--- openweathermap/models/weather-model.js
+++ openweathermap/models/weather-model.js
@@ -13,9 +13,30 @@ var Weather = Arrow.createModel('weather', {
 		},
 		list: {
 			main: {
+				dt: { type: Number, description: 'Time of data forecast', required: true },
+				temp: { type: Number, description: 'Temperature', required: true },
 				temp_min: { type: Number, description: 'Minimum temperature at the moment of calculation', required: true },
-				temp_max: { type: Number, description: 'Maximum temperature at the moment of calculation', required: true }
-		    }
+				temp_max: { type: Number, description: 'Maximum temperature at the moment of calculation', required: true },
+				humidity: { type: Number, description: 'Humidity, %', required: true }
+		    },
+			weather: {
+				main: { type: String, description: 'Group of weather parameters (Rain, Snow, Extreme etc.)', required: true },
+				description: { type: String, description: 'Weather condition within the group', required: true },
+				icon: { type: Number, description: 'Weather icon id', required: true }
+			},
+			clouds: {
+				all: { type: Number, description: 'Cloudiness, %', required: true }
+			},
+			wind: {
+				speed: { type: Number, description: 'Wind speed. Unit Default: meter/sec', required: true },
+				deg: { type: Number, description: 'Wind direction, degrees', required: true }
+			},
+			rain: {
+				volume: { type: Number, description: 'Rain volume for last 3 hours, mm', name: 'volume', required: true }
+			},
+			snow: {
+				snowing: { type: Number, description: 'Snow volume for last 3 hours', name: 'snowing', required: true }
+			}
 		}
 	},
 	connector: 'memory' //'appc.arrowdb'
